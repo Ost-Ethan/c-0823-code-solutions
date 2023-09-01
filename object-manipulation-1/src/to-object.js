8f4d4807da98e74586974a14b28251e7eb1c3817
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-09-01 22:27:35 +0000

    completed exercise

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,14 +1,9 @@
 /* exported toObject */
 function toObject(keyValuePair) {
   // Looking for an array that has 2 elements, a 'string' key and a value
-  // const value = keyValuePair[0];
-  // console.log("Value of value:", value);
 
-  // const key = keyValuePair[1];
-  // console.log("Value of key:", key);
-
-  const combinedObject = keyValuePair[0];
-  combinedObject[keyValuePair[1]] = keyValuePair[1];
+  const combinedObject = {};
+  combinedObject[keyValuePair[0]] = keyValuePair[1];
   console.log('Value of combinedObject:', combinedObject);
 
   return combinedObject;
