6e5c1df63cc9d434cc96dd6d0838296868686cd2
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-09-01 16:43:39 +0000

    in progress

--- object-manipulation-1/src/to-object.js
+++ object-manipulation-1/src/to-object.js
@@ -1,1 +1,15 @@
 /* exported toObject */
+function toObject(keyValuePair) {
+  // Looking for an array that has 2 elements, a 'string' key and a value
+  // const value = keyValuePair[0];
+  // console.log("Value of value:", value);
+
+  // const key = keyValuePair[1];
+  // console.log("Value of key:", key);
+
+  const combinedObject = keyValuePair[0];
+  combinedObject[keyValuePair[1]] = keyValuePair[1];
+  console.log('Value of combinedObject:', combinedObject);
+
+  return combinedObject;
+}
