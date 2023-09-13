9782d9a1084d8e22100011342001e0ba56e6a567
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-09-13 01:40:30 +0000

    completed exercise

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,19 +1,22 @@
 function handleKeyDown(event) {
-  console.log('Value of event.key:', event.key);
+  console.log('Value of event.key', event.key);
+  const key = event.key;
 
-  if (event.key === $spanElements[index]) {
-    $spanElements[index].className('selected-letter right-letter');
+  if (key === $spanElements.item(index).textContent) {
+    $spanElements.item(index).className = 'right-letter';
+    index++;
+    $spanElements.item(index).className = 'selected-letter';
   } else {
-    $spanElements[index].className('selected-letter wrong-letter');
+    $spanElements.item(index).className = 'selected-letter wrong-letter';
   }
 }
 
-const index = 0;
+let index = 0;
 
 const $body = document.querySelector('body');
 const $spanElements = document.querySelectorAll('span');
 const $typingText = document.querySelector('.typing-text');
-console.log('Value of $spanElemnts', $spanElements);
+console.log('item at  $spanElemnts[0]', $spanElements[0]);
 console.log('Value of $typingText', $typingText);
 console.log($spanElements[0]);
 
