d1027cc8305d184d49ff8c948cfc8feb0062b2e3
Ethan Ostrowski <ethan.ostrowski@gmail.com>
2023-10-20 20:56:14 +0000

    completed exercise

--- express-delete/server.ts
+++ express-delete/server.ts
@@ -1,38 +1,53 @@
 import express from 'express';
 
-// type Grade = {
-//   id: number;
-//   name: string;
-//   course: string;
-//   score: number;
-// };
+type Grade = {
+  id: number;
+  name: string;
+  course: string;
+  score: number;
+};
 
-// const grades: Record<number, Grade> = {
-//   12: {
-//     id: 12,
-//     name: 'Tim Berners-Lee',
-//     course: 'HTML',
-//     score: 95,
-//   },
-//   47: {
-//     id: 47,
-//     name: 'Brendan Eich',
-//     course: 'JavaScript',
-//     score: 100,
-//   },
-//   273: {
-//     id: 273,
-//     name: 'Håkon Wium Lie',
-//     course: 'CSS',
-//     score: 92,
-//   },
-// };
+const grades: Record<number, Grade> = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Håkon Wium Lie',
+    course: 'CSS',
+    score: 92,
+  },
+};
 
 const app = express();
 
 app.get('/api/grades', (req, res) => {
+  const gradesArray: Grade[] = [];
+  for (const key in grades) {
+    gradesArray.push(grades[key]);
+  }
   console.log(req.method);
-  res.send('This is the delete assignment.');
+  res.json(gradesArray);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const delId: number = Number(req.params.id);
+  if (grades[delId]) {
+    delete grades[delId];
+    console.log('Deleted entry in grades at index:', req.params.id);
+    res.sendStatus(204);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
 app.listen(8080, () => console.log('Listening on port 8080!'));
